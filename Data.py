Made a few changes: - Fixed the bug with normalization. Previously it was using 0.5 mean and   0.5 std, and now I've modified it to use the means and standard     deviations calculated from the data. This changed the capability of the PyTorchCNN and SpikingCNN to be able to learn. - Made the SpikingCNN a subclass of the PyTorchCNN to reuse code that   allows flexibility between MNIST and the Bloodstain dataset. - Added tracking of the loss from the k fold cross validator. Added a   function for smoothing the training loss over the folds. - Added some more cells to the notebook.

Data.py
@@ -3,6 +3,7 @@ import gdown, shutil, os, logging
 from Transformations import *
 from BloodDataset import BloodDataset
 from torch.utils.data import random_split
+import torch as th
 
 BLOOD_DATASET_FILENAME = 'full_dataset.zip'
 FILE_ID = "1XwfVHx4IU4srDZJLiCFLY8-3UtnwOWxQ"
@@ -16,6 +17,21 @@ def download_data(in_colab=False, using_mnist=False, data_dir='./'):
     else:
         gdown.download(id=FILE_ID)
         shutil.unpack_archive(BLOOD_DATASET_FILENAME, data_dir)
+
+def calculate_mean_and_std(train_set):
+    loader = th.utils.data.DataLoader(train_set, batch_size=1, shuffle=True)
+    mean = 0.
+    std = 0.
+    nb_samples = 0.
+    for data, _ in loader:
+        batch_samples = data.size(0)
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+    mean /= nb_samples
+    std /= nb_samples
+    return mean, std
 
 def get_datasets(in_colab=False, using_mnist=False):
     if in_colab:
@@ -34,8 +50,11 @@ def get_datasets(in_colab=False, using_mnist=False):
         train_set = datasets.MNIST(data_dir, train=True, download=True, transform=mnist_transform)
         test_set = datasets.MNIST(data_dir, train=False, download=True, transform=mnist_transform)
     else:
+        throwaway_blood_dataset = BloodDataset(data_dir, init_transform=resize_transform,
+                                                transform=preprocessing_no_normalize)
+        mean, std = calculate_mean_and_std(throwaway_blood_dataset)
         full_blood_dataset = BloodDataset(data_dir, init_transform=resize_transform, 
-                                          transform=preprocessing, augmentations=augmentations)
+                                          transform=generate_preprocessing_transforms(mean, std), augmentations=augmentations)
         representative_test_set = False
         class_percentages = get_class_percentages(full_blood_dataset)
         while not representative_test_set:

Models.py
@@ -7,68 +7,64 @@ class PyTorchCNN(nn.Module):
     def __init__(self, using_mnist=False):
         self.using_mnist = using_mnist
         if using_mnist:
-          num_classes = 10
+          self.num_classes = 10
+          self.fully_connected_input_size = 16 * 4 * 4
+          conv_layer_kernel_size = 5
         else:
-          num_classes = 2
+          self.num_classes = 2
+          conv_layer_kernel_size = 5
+          self.fully_connected_input_size = 16 * 13 * 13
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, conv_layer_kernel_size)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        if self.using_mnist:
-          self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        else:
-          self.fc1 = nn.Linear(16 * 29 * 29, 120)
+        self.conv2 = nn.Conv2d(6, 16, conv_layer_kernel_size)
+        self.fc1 = nn.Linear(self.fully_connected_input_size, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
+        self.fc3 = nn.Linear(84, self.num_classes)
 
     def forward(self, x):
+        batch_size = x.shape[0]
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         if self.using_mnist:
-          x = x.view(-1, 16 * 4 * 4)
+          x = x.view(batch_size, -1)
         else:
-          x = x.view(-1, 16 * 29 * 29)
+          x = x.view(batch_size, -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
     
-class SpikingCNN(nn.Module):
-    def __init__(self, beta=0.8, num_steps=50):
+class SpikingCNN(PyTorchCNN):
+    def __init__(self, beta=0.8, num_steps=10, **kwargs):
         self.num_steps = num_steps
-        super().__init__()
+        super().__init__(**kwargs)
 
         # Initialize layers
-        self.conv1 = nn.Conv2d(1, 12, 5)
         self.lif1 = snn.Leaky(beta=beta)
-        self.conv2 = nn.Conv2d(12, 64, 5)
         self.lif2 = snn.Leaky(beta=beta)
-        #self.fc1 = nn.Linear(64*29*29, 2)
-        #self.lif3 = snn.Leaky(beta=beta, spike_grad=spike_grad)
-        #self.lif3 = snn.LeakyParallel(input_size=64*29*29, hidden_size=2, beta=beta)
-        self.lif3 = snn.LeakyParallel(input_size=64*4*4, hidden_size=2, beta=beta)
+        self.lif3 = snn.LeakyParallel(input_size=self.fully_connected_input_size, hidden_size=120, beta=beta)
+        self.lif4 = snn.LeakyParallel(input_size=120, hidden_size=self.num_classes, beta=beta)
 
     def forward(self, x):
         batch_size = x.shape[0]
-        spk3_rec = []
-        mem3_rec = []
+        spk4_rec = []
+        mem4_rec = []
 
         # Initialize hidden states and outputs at t=0
         mem1 = self.lif1.init_leaky()
         mem2 = self.lif2.init_leaky()
-        #mem3 = self.lif3.init_leaky()
 
-        for step in range(self.num_steps):
-            cur1 = F.max_pool2d(self.conv1(x), 2)
+        for _ in range(self.num_steps):
+            cur1 = self.pool(self.conv1(x))
             spk1, mem1 = self.lif1(cur1, mem1)
 
-            cur2 = F.max_pool2d(self.conv2(spk1), 2)
+            cur2 = self.pool(self.conv2(spk1))
             spk2, mem2 = self.lif2(cur2, mem2)
 
-            #cur3 = self.fc1(spk2.view(batch_size, -1))
-            #spk3, mem3 = self.lif3(cur3, mem3)
             spk3 = self.lif3(spk2.view(batch_size, -1))
-            spk3_rec.append(spk3)
-            mem3_rec.append(mem2)
+            spk4 = self.lif4(spk3)
+            spk4_rec.append(spk4)
+            mem4_rec.append(mem2)
 
-        return th.stack(spk3_rec, dim=0), th.stack(mem3_rec, dim=0)
+        return th.stack(spk4_rec, dim=0), th.stack(mem4_rec, dim=0)

main.py
@@ -4,48 +4,9 @@ from BloodDataset import BloodDataset
 from Data import get_datasets
 import numpy as np
 import torch as th
-import itertools
 from Models import *
-from TrainingTesting import k_fold_cross_validation
 from utils import *
-
-device = get_device()
-
-def hyperparameter_search(train_set, using_mnist=False, spiking_model=True, num_epochs=5, batch_size=16, k=10):
-    learning_rates = [1e-3, 1e-4, 1e-5]
-    weight_decays = [1e-3, 1e-4, 1e-5]
-
-    hyperparameter_types = [learning_rates, weight_decays]
-    if spiking_model:
-        betas = [0.8, 0.9, 0.95]
-        hyperparameter_types.append(betas)
-    
-    hyperparameter_combinations = list(itertools.product(*hyperparameter_types))
-    logger.debug(f"Hyperparameter combinations: {hyperparameter_combinations}")
-
-    best_accuracy = 0
-    best_hyperparameters = None
-    
-    for i, hyperparameter in enumerate(hyperparameter_combinations):
-        logger.debug(f"Hyperparameter combination {i+1}/{len(hyperparameter_combinations)}: {hyperparameter}")
-        learning_rate = hyperparameter[0]
-        weight_decay = hyperparameter[1]
-        if spiking_model:
-            beta = hyperparameter[2]
-            model = SpikingCNN(beta=beta)
-        else:
-            model = PyTorchCNN(using_mnist=using_mnist)
-        model = model.to(device)
-        optimizer = th.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-        mean_accuracy = k_fold_cross_validation(train_set, model, k=k, num_epochs=num_epochs, optimizer=optimizer, batch_size=batch_size, verbose=True)
-        logger.debug(f"Mean accuracy: {mean_accuracy}")
-        if mean_accuracy > best_accuracy:
-            best_accuracy = mean_accuracy
-            best_hyperparameters = hyperparameter
-
-    logger.debug(f"Best hyperparameters: {best_hyperparameters}")
-    logger.debug(f"Best accuracy: {best_accuracy}")
-    return best_hyperparameters, best_accuracy
+from TrainingTesting import hyperparameter_search
 
 if __name__ == '__main__':
     np.random.seed(0)
@@ -58,8 +19,8 @@ if __name__ == '__main__':
     if os.getenv("COLAB_RELEASE_TAG"):
         in_colab = True
         logger.debug("Running in Colab")
-    USING_MNIST = True
+    USING_MNIST = False
 
     train_set, test_set = get_datasets(in_colab, USING_MNIST)
 
-    best_hyperparameters, best_accuracy = hyperparameter_search(train_set, using_mnist=USING_MNIST, spiking_model=False, num_epochs=5, batch_size=64, k=10)
+    best_hyperparameters, best_accuracy = hyperparameter_search(train_set, using_mnist=USING_MNIST, spiking_model=True, num_epochs=5, batch_size=32, k=10)